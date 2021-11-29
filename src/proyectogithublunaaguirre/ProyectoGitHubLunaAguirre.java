82fd9446bfa256d292434d46d4d3687130662e12
Jamesss022 <Alexx___95@hotmail.com>
2021-11-29 11:35:59 -0600

    Prueba uno.

---
+++ src/proyectogithublunaaguirre/ProyectoGitHubLunaAguirre.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogithublunaaguirre;
+
+/**
+ *
+ * @author Ale_l
+ */
+public class ProyectoGitHubLunaAguirre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("Prueba GitHub"+i);
+        }
+    }
+    
+}
